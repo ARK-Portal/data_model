299887f3f8ecae5c35174994119d5a28f825d07f
jmvera255 <56414911+jmvera255@users.noreply.github.com>
2025-05-28 11:49:54 -0500

    move cat step out of sh to yaml

--- utils/test_json.sh
+++ utils/test_json.sh
@@ -10,5 +10,3 @@ CREDS=./schematic_service_account_creds.json
 # create schematic config yml
 echo "${SCHEMATIC_SERVICE_ACCOUNT_CREDS}" > $CREDS
 echo "✓ Created temp $CREDS from secrets for template generation."
-
-cat schematic_service_account_creds.json
