aad786db52a87e59e15b014cb2545acb1a7ab172
jmvera255 <56414911+jmvera255@users.noreply.github.com>
2025-09-15 16:17:54 -0500

    refactored workflow operational

--- _utils/context_specific_models.py
+++ _utils/context_specific_models.py
@@ -3,6 +3,7 @@ import subprocess
 import os
 import csv
 import sys
+import time
 
 ####
 #### Functions
@@ -29,27 +30,51 @@ for context in os.listdir("model_contexts/"):
   contextCSV = pd.read_csv(f"{path}/ark.{context}_context.csv")
   contextModel = pd.concat([contextCSV, allAttr], ignore_index=True)
   
-  templates = templates + get_model_template_name(contextCSV)
+  templates = get_model_template_name(contextModel)
+  with open(f"{path}/ark.{context}_templates.txt", "w") as f:
+    for t in set(templates):
+      a = f.write(f"{t}\n")
+  f.close()
   
   fid = f"{path}/ark.{context}_model.csv"
   model_fids.append(fid)
   contextModel.to_csv(fid, index=False, quoting=csv.QUOTE_ALL)
 
-# prepare for template generation
-with  open("model_templates/templates.txt", "w") as f:
-  for t in set(templates):
-    a = f.write(f"{t}\n")
-f.close()
+print("\nAll context model csv files created!\n")
 
 # run schematic schema convert on each model csv
-for fid in model_fids:
-#schematic schema convert ark.model.csv
-result = subprocess.run(["schematic", "schema", "convert", fid], capture_output=True, text=True, check=True)
-print(result.stdout)
-if result.stderr:
-  print(f"Error: {result.stderr}")
-
+for context in os.listdir("model_contexts/"):
+  path = f"model_contexts/{context}"
+  model_csv = f"{path}/ark.{context}_model.csv"
+  model_jsonld = f"{path}/ark.{context}_model.jsonld"
+  #schematic schema convert ark.model.csv
+  args = ["schematic", "schema", "convert", model_csv]
+  result = subprocess.run(args, capture_output=True, text=True, check=True)
+  print(result.stdout)
+  if result.stderr:
+    print(f"Error: {result.stderr}")
+  
+  # make csv templates for each context model
+  with open(f"{path}/ark.{context}_templates.txt", "r") as f:
+    templates = [t.rstrip('\n') for t in f.readlines()]
+  for t in templates:
+    out_csv = f"model_templates/ark.{t}.csv"
+    out_json = f"model_json_schema/ark.{t}.schema.json"
+    args = ["schematic", "manifest", "-c", "schematic_config.yml", "get", "-dt", t, "-p", model_jsonld, "-o", out_csv]
+    result = subprocess.run(args, capture_output=True, text=True, check=True)
+    print(result.stdout)
+    if result.stderr:
+      print(f"Error: {result.stderr}")
+    
+    # manage json schema output
+    orig = f"{path}/ark.{context}_model.{t}.schema.json"
+    os.rename(orig, out_json)
+    
+    # sleep for 5 seconds to keep google API from complaining
+    time.sleep(5)
 
 print("\nDone!\n")
+
+
 
 # END
