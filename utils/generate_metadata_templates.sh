02cca2ab6706b3fb672886926c8a239c722f57f6
jmvera255 <56414911+jmvera255@users.noreply.github.com>
2025-10-31 14:42:12 -0500

    move rm step to workflow to enable local execution of util script

--- utils/generate_metadata_templates.sh
+++ utils/generate_metadata_templates.sh
@@ -9,28 +9,26 @@ echo "${SCHEMATIC_SERVICE_ACCOUNT_CREDS}" > schematic_service_account_creds.json
 echo "✓ Created schematic_service_account_creds.json from secrets for template generation."
 
 # generate list of templates defined in model
-cut -f1 -d',' ark.model.csv | grep -i template > metadata_templates/templates.txt
-sed -i 's/ //g' metadata_templates/templates.txt
-sed -i 's/"//g' metadata_templates/templates.txt
-sed -i 's/\([[:alpha:]]\)/\U\1/' metadata_templates/templates.txt
-cat metadata_templates/templates.txt
-echo "✓ Created metadata_templates/templates.txt."
+cut -f1 -d',' ark.model.csv | grep -i template > model_templates/templates.txt
+sed -i 's/ //g' model_templates/templates.txt
+sed -i 's/"//g' model_templates/templates.txt
+sed -i 's/\([[:alpha:]]\)/\U\1/' model_templates/templates.txt
+cat model_templates/templates.txt
+echo "✓ Created model_templates/templates.txt."
 
 # use schematic to generate xlsx for each template in templates.txt
 while read template; do
-  schematic manifest -c schematic_config.yml get -dt $template -oxlsx metadata_templates/$template.xlsx
+  schematic manifest -c schematic_config.yml get -dt $template -oxlsx model_templates/$template.xlsx
   
   sleep 10 # prevent google API from complaining
   
-done < metadata_templates/templates.txt
+done < model_templates/templates.txt
 echo "✓ Created xlsx templates created."
 
 # clean up/reorg remaining output from manifest get
-mv *.schema.json metadata_templates/
-echo "json schemas moved to metadata_templates/"
-
-# clean up and remove sensitive info so it's not commited to repo
-echo "Cleaning up directory"
-rm -f schematic_service_account_creds.json
+mv *.schema.json model_templates/
+echo "json schemas moved to model_templates/"
 
 echo "✓ Done!"
+
+# END
