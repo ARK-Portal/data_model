284e8be3a737b00b7bd6ff2a8834c86e5db37067
jmvera255 <56414911+jmvera255@users.noreply.github.com>
2025-11-03 11:24:19 -0600

    test branch subdir download workflow for accessing model files during workflow execution since more than just the model csv file will be needed for accurate template rendering on dictionary site

--- _utils/update_site_content.R
+++ _utils/update_site_content.R
@@ -1,13 +1,12 @@
 #! Rscript
 
 # install remote package
-remotes::install_github("Sage-Bionetworks/JustTheDocsDataDictionary", ref = "contexts_refactor")
+remotes::install_github("Sage-Bionetworks/JustTheDocsDataDictionary", ref = "dev")
 # load installed library
 library(JustTheDocsDataDictionary)
 
-args <- commandArgs(trailingOnly = TRUE)
-
 # generate site content
-main(args[1])
+#main(args[1])
+main(portal = "ark", template_list = "model_templates/templates.txt", branch = "main")
 
 # END
