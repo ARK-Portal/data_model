4fc783174070b41f04df5998ed9c70f422f9c932
jmvera255 <56414911+jmvera255@users.noreply.github.com>
2026-02-02 13:48:44 -0600

    local testing complete

--- utils/generate_jsonschema.py
+++ utils/generate_jsonschema.py
@@ -3,7 +3,7 @@ from synapseclient.extensions.curator import generate_jsonschema
 import pandas as pd
 
 '''
-
+use synapseclient extension to create Curator json schema from context models
 '''
 
 # create synapse client obj, this will be unnecessary in future client releases
@@ -16,24 +16,16 @@ templates = templates.groupby(['context']).agg({'template': lambda x: list(x)}).
 templates = templates.set_index('context').to_dict()['template']
 
 for context in templates.keys():
-    print(f"Generating JSON schemas for context: {context}")
-    for t in templates[context]:
-      try:
-        schemas, file_paths = generate_jsonschema(
-          data_model_source=f"model_contexts/{context}/ark.{context}_model.csv",
-          output=f"model_json_schema/ark.{t}.schema.json",
-          data_types= [t],
-          synapse_client=syn
-        )
-        print("Success!")
-      except Exception as e:
-        print(f"Error generating schema for template '{t}' in context '{context}': {e}")
+  print(f"Generating JSON schemas for context: {context}")
+  for t in templates[context]:
+    schemas, file_paths = generate_jsonschema(
+      data_model_source=f"model_contexts/{context}/ark.{context}_model.csv",
+      output=f"model_json_schema/ark.{t}.schema.json",
+      data_types= [t],
+      synapse_client=syn,
+      data_model_labels = "display_label"
+    )
 
+print("JSON schema generation complete!")
 
-schemas, file_paths = generate_jsonschema(data_model_source= "model_contexts/clinical/ark.clinical_model.csv",
-                                          output= "model_json_schema/ark.test.schema.json",
-                                          data_types= ['ClinicalMetadataTemplate'],
-                                          synapse_client= syn
-                                          )
-test = pd.read_csv("model_contexts/clinical/ark.clinical_model.csv")
-                                          
+# END
